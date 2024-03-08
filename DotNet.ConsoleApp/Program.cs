2a7dab37110e228174cbfa1018e56c3966d8f2e8
Chaw Su Han <chawsuhan.csh415@gmail.com>
2024-03-08 14:37:17 +0630

    Add project files.

---
+++ DotNet.ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//ConnectionStringBuilder
+using System.Data;
+using System.Data.SqlClient;
+
+//Using Query
+//SqlConnectionStringBuilder Build = new SqlConnectionStringBuilder();
+//Build.DataSource = ".";
+//Build.InitialCatalog = "DotNet";
+//Build.UserID = "sa";
+//Build.Password = "sasa@123";
+
+//string query = "select * from DotNetT";//Get data from table
+//SqlConnection Connect = new SqlConnection(Build.ConnectionString);//Connect Database
+
+//Connect.Open();//DataBase Open 
+//Connect.Close();//Database Close
+
+//SqlCommand Cmd = new SqlCommand(query, Connect);
+//SqlDataAdapter Adapter = new SqlDataAdapter(Cmd);
+//DataTable Table = new DataTable();
+//Adapter.Fill(Table);
+//foreach(DataRow DR in Table.Rows)
+//{
+//    Console.WriteLine(DR["Id"]);
+//    Console.WriteLine(DR["Name"]);
+//    Console.WriteLine(DR["Address"]);
+//    Console.WriteLine(DR["Description"]);
+// }
+
+//Using SQL Sentences
+SqlConnectionStringBuilder Build = new SqlConnectionStringBuilder();
+Build.DataSource = ".";
+Build.InitialCatalog = "DotNet";
+Build.UserID = "sa";
+Build.Password = "sasa@123";
+
+string query = @"SELECT [Id]
+      ,[Name]
+      ,[Address]
+      ,[Description]
+  FROM [dbo].[DotNetT]";//Get data from table
+SqlConnection Connect = new SqlConnection(Build.ConnectionString);//Connect Database
+
+Connect.Open();//DataBase Open 
+Connect.Close();//Database Close
+
+SqlCommand Cmd = new SqlCommand(query, Connect);
+SqlDataAdapter Adapter = new SqlDataAdapter(Cmd);
+DataTable Table = new DataTable();
+Adapter.Fill(Table);
+foreach (DataRow DR in Table.Rows)
+{
+    Console.WriteLine(DR["Id"]);
+    Console.WriteLine(DR["Name"]);
+    Console.WriteLine(DR["Address"]);
+    Console.WriteLine(DR["Description"]);
+}
