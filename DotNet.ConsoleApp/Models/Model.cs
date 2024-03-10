02f2b1ed6be482de4dae9cab73a8ca385990154b
Chaw Su Han <chawsuhan.csh415@gmail.com>
2024-03-10 19:15:03 +0630

    EF Core

--- DotNet.ConsoleApp/Models/Model.cs
+++ DotNet.ConsoleApp/Models/Model.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DotNet.ConsoleApp.Models
 {
+    [Table("DotNetT")]
     public class Model
     {
+       
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
 
-
+      
 
     }
 }
