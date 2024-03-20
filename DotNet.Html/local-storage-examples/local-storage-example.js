9807b3f49851979ccec98c507a149db3264e9c8f
Chaw Su Han <chawsuhan.csh415@gmail.com>
2024-03-20 10:58:39 +0630

    JS Create Function

--- DotNet.Html/local-storage-examples/local-storage-example.js
+++ DotNet.Html/local-storage-examples/local-storage-example.js
@@ -2,7 +2,7 @@ const dotnetT = 'DotNetT';
 
 runBlog();
 function runBlog() {
-createBlog('Test3','Test4','Test5');
+    createBlog('Test3', 'Test4', 'Test5');
 }
 function readBlog() {
 
@@ -11,14 +11,21 @@ function editBlog() {
 
 }
 function createBlog(name, address, description) {
+    let lstBlogs = [];//Array
+    let blogStr = localStorage.getItem(dotnetT);
+    if (blogStr != null) {
+        lstBlogs = JSON.parse(blogStr);
+    }
     const blog = {
+        Id: uuidv4(),
         Name: name,
         Address: address,
         Description: description
     }
-    let jsonStr=JSON.stringify(blog);
-
-    localStorage.setItem(dotnetT,jsonStr);
+    lstBlogs.push(blog);
+    //let jsonStr = JSON.stringify(blog);
+    let jsonStr = JSON.stringify(lstBlogs);
+    localStorage.setItem(dotnetT, jsonStr);
 
 
 }
@@ -28,4 +35,8 @@ function updateBlog() {
 function deleteBlog() {
 
 }
-
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
